960801c47c43dab22a3b34cf1d1fc9363ed65a5b
Commander thrashdin <aun.sokolov@gmail.com>
2022-03-25 14:40:55 +0200

    Overhaul to make a proper script

--- profiler.py
+++ profiler.py
@@ -1,40 +1,59 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
 import pandas as pd
 import matplotlib.pyplot as plt
-dffirst = pd.read_csv("input.txt", sep='|', 
+import argparse
+
+
+def read_queries(path):
+    df = pd.read_csv(path, sep='|',
                        dtype='str', na_filter=False,
                        header = None).iloc[:, 1:-1]
-dffirst = dffirst.applymap(lambda x: float(x[:-4]))
+    return df.applymap(lambda x: float(x[:-4]))
 
-dfsecond = pd.read_csv("input2.txt", sep='|', 
-                      dtype='str', na_filter=False,
-                      header = None).iloc[:, 1:-1]
-dfsecond = dfsecond.applymap(lambda x: float(x[:-4]))
+def normalize(df):
+    return df.reindex(df.mean().sort_values().index, axis=1)
 
-
-diff = dffirst.subtract(dfsecond)
-diff = diff.transpose()
-diff = diff.reindex(diff.mean().sort_values().index, axis=1)
-means = diff.mean()
-mins = diff.min()
+def plot_show(df):
+   df.plot(kind='box')
+   plt.show()
 
 
-diff.plot(kind='box')
 
-means.plot(kind='box')
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Compare execution time of mdb queries')
 
-mins.plot(kind='box')
-
-
-diff_norm = diff.apply(lambda x: x / max(x.min(), x.max(), key=abs))
-diff_norm = diff_norm.reindex(diff_norm.mean().sort_values().index, axis=1)
-diff_norm
-
-diff_norm.plot(kind='box')
+    parser.add_argument('first_file', type=str,
+                      help='Name of the first file')
+    parser.add_argument('second_file', type=str,
+                      help='Name of the second file')
+    args = parser.parse_args()
 
 
-div_diff = dffirst.divide(dfsecond)
-div_diff = div_diff.transpose()
-div_diff = div_diff.reindex(div_diff.mean().sort_values().index, axis=1)
+    dffirst = read_queries(args.first_file)
+    dfsecond = read_queries(args.second_file)
 
-div_diff.plot(kind='box')
 
+
+    diff = dffirst.subtract(dfsecond)
+    diff = diff.transpose()
+    diff = normalize(diff)
+
+    means = diff.mean()
+
+    mins = diff.min()
+
+    diff_norm = diff.apply(lambda x: x / max(x.min(), x.max(), key=abs))
+    diff_norm = normalize(diff_norm)
+
+    div_diff = dffirst.divide(dfsecond)
+    div_diff = div_diff.transpose()
+    div_diff = normalize(div_diff)
+
+    plot_show(diff)
+    plot_show(means)
+    plot_show(mins)
+    plot_show(diff_norm)
+    plot_show(div_diff)
